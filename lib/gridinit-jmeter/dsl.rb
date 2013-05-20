7742c3736a1d23b9688c4483c5b888401dde9875
Tim Koopmans <tim.koops@gmail.com>
2013-05-20 16:58:23 +1000

    add clear_each_iteration params

--- lib/gridinit-jmeter/dsl.rb
+++ lib/gridinit-jmeter/dsl.rb
@@ -36,7 +36,17 @@ module Gridinit
 
       alias_method :defaults, :http_request_defaults      
 
+      def http_cookie_manager(params={}, &block)
+        params[:clearEachIteration] = true if params.keys.include? 'clear_each_iteration'
+        super
+      end
+
       alias_method :cookies, :http_cookie_manager
+
+      def http_cache_manager(params={}, &block)
+        params[:clearEachIteration] = true if params.keys.include? 'clear_each_iteration'
+        super
+      end
 
       alias_method :cache, :http_cache_manager
 
